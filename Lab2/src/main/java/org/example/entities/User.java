5f0294eaa9119eb2bc5feb4356e434df25a3363a
GeniusDP <zaranikbz@gmail.com>
2022-11-08 11:05:16 +0200

    Revert "reduced number of USer entity fields"
    This reverts commit 9a41d4ec64a01002ab5a7acd605090ad4ef82058.

--- Lab2/src/main/java/org/example/entities/User.java
+++ Lab2/src/main/java/org/example/entities/User.java
@@ -1,5 +1,6 @@
 package org.example.entities;
 
+import java.time.LocalDate;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,11 +16,21 @@ public class User {
 
   private String password;
 
+  private String firstName;
+
+  private String lastName;
+
+  private LocalDate dateOfBirth;
+
   private Role role;
 
-  public User(String username, String password, Role role) {
+  public User(String username, String password, String firstName, String lastName,
+      LocalDate dateOfBirth, Role role) {
     this.username = username;
     this.password = password;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.dateOfBirth = dateOfBirth;
     this.role = role;
   }
 }
