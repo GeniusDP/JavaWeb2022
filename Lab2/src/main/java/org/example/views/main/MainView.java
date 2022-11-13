63289d7606f8b758e5c73c7e40b51de25294daf6
GeniusDP <zaranikbz@gmail.com>
2022-11-13 21:56:18 +0200

    refactored MainView.java

--- Lab2/src/main/java/org/example/views/main/MainView.java
+++ Lab2/src/main/java/org/example/views/main/MainView.java
@@ -4,14 +4,14 @@ import java.util.Scanner;
 
 public class MainView {
 
-  private Scanner scanner = new Scanner(System.in);
+  private final Scanner scanner = new Scanner(System.in);
 
   public void unauthorizedMessage() {
     System.out.println("Bad input data: performing login/register operation was NOT SUCCESSFUL.");
   }
 
   public boolean askForRepeat() {
-    System.out.println("Repeat? Write YES of NO.");
+    System.out.println("Go to menu another one time? Write YES of NO.");
     return scanner.nextLine().equals("YES");
   }
 
