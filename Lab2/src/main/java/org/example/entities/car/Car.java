428a9478c91e5a48abdc05fafb1932050c5bb2c3
GeniusDP <zaranikbz@gmail.com>
2022-11-14 14:47:29 +0200

    car builder

--- Lab2/src/main/java/org/example/entities/car/Car.java
+++ Lab2/src/main/java/org/example/entities/car/Car.java
@@ -1,10 +1,10 @@
 package org.example.entities.car;
 
-import lombok.Builder;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
-@Builder
+@AllArgsConstructor
 public class Car {
 
   private Long id;
@@ -17,9 +17,60 @@ public class Car {
 
   private Integer basePrice;
 
+
+  public static CarBuilder builder() {
+    return new CarBuilder();
+  }
+
+  public static class CarBuilder {
+    
+    private Long id;
+
+    private Mark mark;
+
+    private QualityClass qualityClass;
+
+    private String name;
+
+    private Integer basePrice;
+
+
+    public CarBuilder id(Long id){
+      this.id = id;
+      return this;
+    }
+
+    public CarBuilder mark(Mark mark){
+      this.mark = mark;
+      return this;
+    }
+
+    public CarBuilder qualityClass(QualityClass qualityClass){
+      this.qualityClass = qualityClass;
+      return this;
+    }
+
+    public CarBuilder name(String name){
+      this.name = name;
+      return this;
+    }
+
+
+    public CarBuilder basePrice(int basePrice){
+      this.basePrice = basePrice;
+      return this;
+    }
+    
+    public Car build(){
+      return new Car(id, mark, qualityClass, name, basePrice);
+    }
+
+  }
+
+
   @Override
   public String toString() {
     return String.format("Car(id=%3s, mark=%30s, qualityClass=%10s, name=%15s, basePrice=%10s)",
-            id, mark, qualityClass, name, basePrice);
+        id, mark, qualityClass, name, basePrice);
   }
 }
